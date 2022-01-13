2be3c33754b65ca105640f124507b9266a1012c8
RuBrock <r.brocksilva@yahoo.com.br>
2022-01-13 15:15:42 -0300

    App1 project configs and structure

--- app1/src/pages/index.js
+++ app1/src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Nav from '../components/nav';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,7 +12,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Nav />
+      
       <main className={styles.main}>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
